ff9a4728541ca1d330159ce70544e4b7ac54d8a1
dungtm-1343 <tran.manh.dung@sun-asterisk.com>
2024-05-08 13:47:16 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by email: params.dig(:session, :email)&.downcase
-    if @user&.authenticate(params.dig(:session, :password))
+    if @user.try(:authenticate, params.dig(:session, :password))
       reset_session
-      log_in @user
+      remember @user
       redirect_to @user, status: :see_other
     else
       flash.now[:danger] = t "invalid_login"
