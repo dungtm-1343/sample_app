bd7384a3644978357dc605587bc1421e1fa37576
dungtm-1343 <tran.manh.dung@sun-asterisk.com>
2024-05-10 17:16:16 +0700

    Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   ATTRIBUTES = %i[name email password password_confirmation].freeze
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   validates :name, presence: true, length: { maximum: Settings.users.max_length_name }
   validates :email, presence: true, length: { maximum: Settings.users.max_length_email },
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :downcase_email
+  before_create :create_activation_digest
 
   scope :order_by_name, -> { order(:name) }
 
@@ -37,13 +38,30 @@ class User < ApplicationRecord
     update_column :remember_digest, nil
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_mail
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.create!(name: "Admin", email: "adminreal@adm.com", password: "adminadmin", password_confirmation: "adminadmin",
-             admin: true)
+             admin: true, activated: true, activated_at: Time.zone.now)
 
 30.times do |n|
   name = Faker::Name.name
-  email = "example-#{n + 100}@railstutorial.org"
+  email = "example-#{n + 4000}@railstutorial.org"
   password = "madurc.29"
   User.create!(name:, email:, password:, password_confirmation: password)
 end
